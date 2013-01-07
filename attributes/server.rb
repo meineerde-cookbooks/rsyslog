7e2e437e5c7d9217e5b94f0e61491e44c119da17
Holger Just <hjust@meine-er.de>
2013-01-07 13:44:27 +0100

    Group attributes more intuitively

--- attributes/server.rb
+++ attributes/server.rb
@@ -1,9 +1,9 @@
-default["rsyslog"]["server"]["log_dir"]       = "/srv/rsyslog"
 default["rsyslog"]["server"]["protocols"]     = ["tcp", "udp"]
 default["rsyslog"]["server"]["tcp_port"]      = 514
 default["rsyslog"]["server"]["udp_port"]      = 514
 default["rsyslog"]["server"]["tls_port"]      = 514
 
+default["rsyslog"]["server"]["log_dir"]       = "/srv/rsyslog"
 default["rsyslog"]["server"]["per_host_dir"]  = "%HOSTNAME%/%$YEAR%/%$MONTH%/%$DAY%"
 # set to false to disable creating per host logs on a server
 default["rsyslog"]["server"]["per_host_logs"] = true
