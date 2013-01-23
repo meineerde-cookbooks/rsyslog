c1bd0adb650fe3ff480cb5a1174f1c83a7193cf4
Holger Just <hjust@meine-er.de>
2013-01-23 17:49:03 +0100

    Allow to specify the template for forwarded logs

--- attributes/client.rb
+++ attributes/client.rb
@@ -5,4 +5,5 @@ default["rsyslog"]["client"]["server_port"]   = 514
 default["rsyslog"]["client"]["server_ip"]     = nil
 default["rsyslog"]["client"]["server_name"]   = nil # required for tls protocol under Chef solo
 default["rsyslog"]["client"]["server_search"] = "role:loghost"
+default["rsyslog"]["client"]["forward_template"] = "RSYSLOG_TraditionalFileFormat"
 
